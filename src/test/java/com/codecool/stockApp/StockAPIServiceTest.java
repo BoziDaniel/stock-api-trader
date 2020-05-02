10471a9157598157442fc4c119c3a5e3998d5a9a
BoziDaniel <daniel.bozi88@gmail.com>
2020-05-02 18:29:31 +0200

    second test passes

--- src/test/java/com/codecool/stockApp/StockAPIServiceTest.java
+++ src/test/java/com/codecool/stockApp/StockAPIServiceTest.java
@@ -1,7 +1,6 @@
 package com.codecool.stockApp;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import java.io.IOException;
@@ -12,21 +11,24 @@ import static org.mockito.Mockito.when;
 
 // TODO
 class StockAPIServiceTest {
-    public static final String url = "https://financialmodelingprep.com/api/v3/stock/real-time-price/aapl";
+    public static final String URL = "https://financialmodelingprep.com/api/v3/stock/real-time-price/aapl";
+    RemoteURLReader remoteURLReader = Mockito.mock(RemoteURLReader.class);
+    StockAPIService stockAPIService = new StockAPIService(remoteURLReader);
     @Test // everything works
     void testGetPriceNormalValues() throws IOException {
-
-        RemoteURLReader remoteURLReader = Mockito.mock(RemoteURLReader.class);
-        StockAPIService stockAPIService = new StockAPIService(remoteURLReader);
-        when(remoteURLReader.readFromUrl(url)).thenReturn("{'price': 289.14}");
+        when(remoteURLReader.readFromUrl(URL)).thenReturn("{'price': 289.14}");
         assertEquals(289.14, stockAPIService.getPrice("aapl"));
     }
 
     @Test // readFromURL threw an exception
-    void testGetPriceServerDown() {
+    void testGetPriceServerDown() throws IOException {
+        when(remoteURLReader.readFromUrl(URL)).thenThrow(new IOException());
+        assertThrows(IOException.class, () -> stockAPIService.getPrice("aapl"));
 
     }
-
+//    Exception exception = assertThrows(ArithmeticException.class, () ->
+//            calculator.divide(1, 0));
+//    assertEquals("/ by zero", exception.getMessage());
     @Test // readFromURL returned wrong JSON
     void testGetPriceMalformedResponse() {
 
